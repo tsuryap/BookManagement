b8445fb60947f618d457a5477ac75824477a0399
User1987031987 <user19870319@gmail.com>
2017-06-18 09:11:21 +0530

    new changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,7 @@ module.exports = function(grunt) {
                     "bower_components/lodash/lodash.js",
                     "bower_components/angular/angular.js",
                     "bower_components/angular-ui-router/release/angular-ui-router.js",
+                    "bower_components/angular-messages/angular-messages.js",
                     "app/app.js",
                     "app/**/*.js",
                     "temp/all-templates.js"
--- app/components/admin/addBook/AddBookCtrl.js
+++ app/components/admin/addBook/AddBookCtrl.js
@@ -1,11 +1,25 @@
 app.controller('AddBook', ['$scope', 'Constants', function($scope, Constants) {
     $scope.bookHeading = Constants.admin.addBook.heading;
     $scope.bookObj = {};
-    $scope.addBook = function() {
+    $scope.addBook = function(bookform) {
         console.log($scope.bookObj);
+        debugger;
+        if (!bookform.$valid) {
+            showValidationErrors(bookform, true);
+            return false;
+        }
     }
-    $scope.reset = function() {
+    $scope.reset = function(bookform) {
+        showValidationErrors(bookform, false);
         $scope.bookObj = {};
     };
+
+    function showValidationErrors(bookform, touched) {
+        _.each(bookform.$error, function(value, key) {
+            _.each(bookform.$error[key], function(value) {
+                value.$touched = touched;
+            });
+        });
+    }
 
 }]);
